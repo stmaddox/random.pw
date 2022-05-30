cb602145ae66b8f36d0bf80faf9b22044487a0fc
stmaddox <sean.maddox89@yahoo.com>
2022-05-30 15:47:34 -0700

    trying to add for loop

--- Develop/script.js
+++ Develop/script.js
@@ -2,52 +2,68 @@
 var lowLetter = "abcdefghijklmnopqrstuvwxyz"
 var upLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var number = "0123456789"
-var specialChar ="!@#$%^&*()_-=+><.,"
+var specialChar = "!@#$%^&*()_-=+><.,"
 var finalPassword = ""
-var generatePassword = function() {
+var pickLow = function() {
+  return Math.floor(Math.random()*lowLetter.length)
+};
+var pickUp = function() {
+  return Math.floor(Math.random()* upLetter.length)
+};
+var pickNum = function() {
+  return Math.floor(Math.random()* number.length)
+};
+var pickSpecChar = function() {
+  return Math.floor(Math.random()* specialChar.length)
+};
+
+
+var generatePassword = function () {
   //step 1. click button to generate password
   //Step 2. When we click it prompts length of password 8-128 characters
   //Step 3. Choose if password needs lowercase letter, uppercase letter, numbers, and/or special characters
   //Step 4. Prompt whether at least one character type has been selected 
   //Step 5. Generate password that matches the criteria
   //Step 6. Display the password generated either in an alert or written to the page 
-var passwordLength = prompt("Please pick a password length between 8 and 128 characters");
-  console.log(passwordLength);  
+  var passwordLength = prompt("Please pick a password length between 8 and 128 characters");
+  console.log(passwordLength);
   //for(i=0; i < passwordLength; i++)
-    if (passwordLength >= 8 && passwordLength <=128){
-      var lowLetterCheck = confirm("Does your password need lowercase letters?");
-      console.log(lowLetterCheck);
-      var upLetterCheck = confirm("Does your password need uppercase letters?");
-      console.log(upLetterCheck);
-      var numCheck = confirm("Does your password need numbers?")
-      console.log(numCheck);
-      var specialCharCheck = confirm("Does your password need special characters?")
-      console.log(specialCharCheck);
-      if (lowLetterCheck === true){
-        var pickLow = Math.floor(Math.random()*lowLetter.length)
-        finalPassword = finalPassword + lowLetter.charAt(pickLow)
-      }
-      if (upLetterCheck === true){
-        var pickUp = Math.floor(Math.random()*upLetter.length)
-        finalPassword = finalPassword + upLetter.charAt(pickUp)
-      }
-      if (numCheck === true){
-        var pickNum = Math.floor(Math.random()*number.length)
-        finalPassword = finalPassword + number.charAt(pickNum)
-      }
-      if (specialCharCheck ===  true){
-        var pickSpecChar = Math.floor(Math.random()*specialChar.length)
-        finalPassword = finalPassword + specialChar.charAt(pickSpecChar)
-      }
-  }  else{
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    var lowLetterCheck = confirm("Does your password need lowercase letters?");
+    console.log(lowLetterCheck);
+    var upLetterCheck = confirm("Does your password need uppercase letters?");
+    console.log(upLetterCheck);
+    var numCheck = confirm("Does your password need numbers?")
+    console.log(numCheck);
+    var specialCharCheck = confirm("Does your password need special characters?")
+    console.log(specialCharCheck);
+    if (lowLetterCheck === true) {
+      var pickLow = Math.floor(Math.random() * lowLetter.length)
+      arrPassword.push(pickLow[i])
+    }
+    if (upLetterCheck === true) {
+      var pickUp = Math.floor(Math.random() * upLetter.length)
+     arrPassword.push(pickUp[i])
+    }
+    if (numCheck === true) {
+      var pickNum = Math.floor(Math.random() * number.length)
+      arrPassword.push(pickNum[i])
+    }
+    if (specialCharCheck === true) {
+      var pickSpecChar = Math.floor(Math.random() * specialChar.length)
+      arrPassword.push(pickSpecChar[i])
+    }
+    }  else {
     alert("Please choose a password length between 8 and 128 characters")
-  }  
-  for(let i=0; finalPassword < passwordLength;i++) {
-    let generate = [{pickLow} + {pickUp} + {pickNum} + {pickSpecChar}]; 
-    finalPassword += generate;
-  } 
-  
-  return finalPassword
+    }   
+var arrPassword = []
+
+for(i=0; i < passwordLength; i++) {
+  var randomChar = arrPassword[Math.floor(Math.random()*arrPassword.length)]()
+  finalPassword += randomChar
+};
+
+return finalPassword
   
 };
 
